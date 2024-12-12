bb64b0c0679b13905ec4e0c857cab3a6c851fd1c
AnaJulia1959 <anajuliarodrigues1959@gmail.com>
2024-12-12 20:03:04 -0300

    commit

---
+++ Atividade2/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class activity2 extends StatefulWidget {
+  const activity2({super.key});
+
+  @override
+  State<activity2> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<activity2> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
